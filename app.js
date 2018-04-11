84b5918e149237552a1a7d840b8337b678894cf4
ADEYEMI IFEDAYO O <adeyemiife@ymail.com>
2018-04-11 14:09:48 -0700

    made by me

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const logger = require("morgan");
 const bodyparser = require("body-parser");
 const mysql = require("mysql");
-const route = require("./routes/index");
+const Route = require("./routes/index");
 
 var app = express();
 
@@ -14,20 +14,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Require our routes into the application.
 Route(app);
 
-app.get('/', (req, res) =>
-res.status(202).send({ message: 'This API supplies student details' }));
+app.get('/', function(req, res) {
+    res.status(202).send({ message: 'This API supplies student details' })
+});
 
-app.get('*', (request, response) => response.status(404).send({
-  message: 'INVALID ROUTE!!!.',
-}));
-app.post('*', (request, response) => response.status(404).send({
-  message: 'INVALID ROUTE!!!.',
-}));
-app.put('*', (request, response) => response.status(404).send({
-  message: 'INVALID ROUTE!!!.',
-}));
-app.delete('*', (request, response) => response.status(404).send({
-  message: 'INVALID ROUTE!!!.',
-}));
+app.get('*', function (req, res){
+     response.status(404).send({message: 'The Route is invalid',})
+});
 
-export default app;
+app.post('*', function (req, res){ 
+    response.status(404).send({message: 'The Route is invalid',})
+});
+
+app.put('*', function (req, res) {
+    response.status(404).send({message: 'The Route is invalid',})
+});
+
+app.delete('*', function (req, res){ 
+    response.status(404).send({message: 'The Route is invalid',})
+});
+
+module.exports = app;
