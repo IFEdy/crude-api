d8d4d611affeb31f4d096c1363949c827be34471
ADEYEMI IFEDAYO O <adeyemiife@ymail.com>
2018-04-11 14:16:22 -0700

    made by me

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const StudentController = require("./controller/students");
+
+
+const Route = function(app){
+
+  app.post('/api/students',  StudentController.create);
+  app.get('/api/students', StudentController.getAllStudents);
+  app.get('/api/students/:id',  StudentController.getOneStudent);
+  app.put('/api/students/:id',  StudentController.updateStudent);
+  app.delete('/api/students/:id', StudentController.deletestudent);
+
+};
+
+module.exports = Route;
